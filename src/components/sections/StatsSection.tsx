66a437b614fc303d52d6bccc0a565526aac1e260
kiraberser <121148745+kiraberser@users.noreply.github.com>
2025-10-29 03:26:49 -0600

    style: grid

--- src/components/sections/StatsSection.tsx
+++ src/components/sections/StatsSection.tsx
@@ -39,7 +39,7 @@ const StatsSection = () => {
           </p>
         </div>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
           {stats.map((stat, index) => {
             const IconComponent = stat.icon;
             return (
